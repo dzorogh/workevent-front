162613864bc5fa53028378be88aa19bae5e18a18
Sergey Indenbom <dzorogh@gmail.com>
2024-12-14 11:27:03 +0300

    feat: bundle analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+const nextConfig: NextConfig = withBundleAnalyzer({
   output: "standalone",
   experimental: {
     cssChunking: 'loose',
@@ -47,6 +51,6 @@ const nextConfig: NextConfig = {
 
     return config
   },
-};
+});
 
 export default nextConfig;
