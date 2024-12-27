b9b3ebb757bea230cf92bc542e441f5eba16e5cf
Sergey Indenbom <dzorogh@gmail.com>
2024-12-27 21:57:53 +0300

    refactor: update schedule page to handle params as promises and improve metadata generation
    - Changed the Props type to use a Promise for params, ensuring asynchronous handling of year data.
    - Updated the generateMetadata and SchedulePage functions to await params, enhancing clarity and consistency in year handling.
    - Adjusted the title and event date calculations to reflect the new year variable, improving the overall structure and readability of the code.

--- src/app/schedule/[year]/page.tsx
+++ src/app/schedule/[year]/page.tsx
@@ -8,9 +8,9 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 
 type Props = {
-    params: {
+    params: Promise<{
         year: string
-    }
+    }>,
     searchParams: {
         industry_id?: string
     }
@@ -33,8 +33,9 @@ export async function generateMetadata(
     { params, searchParams }: Props,
     parent: ResolvingMetadata
 ): Promise<Metadata> {
-    const page = await getPage(params.year);
-    const title = 'Расписание (план) мероприятий на ' + params.year + ' год' + ' — ' + (await parent).title?.absolute;
+    const year = (await params).year;
+    const page = await getPage(year);
+    const title = 'Расписание (план) мероприятий на ' + year + ' год' + ' — ' + (await parent).title?.absolute;
 
     return {
         title: title,
@@ -43,7 +44,8 @@ export async function generateMetadata(
 }
 
 export default async function SchedulePage({ params, searchParams }: Props) {
-    const page = await getPage(params.year);
+    const year = (await params).year;
+    const page = await getPage(year);
 
     // Compile the MDX source code to a function body
     const code = String(
@@ -58,8 +60,8 @@ export default async function SchedulePage({ params, searchParams }: Props) {
 
     const industries = await Api.GET('/v1/industries');
     const requestParams = {
-        date_from: new Date(Number(params.year), 0, 1, 0, 0, 0, 0).getTime() / 1000,
-        date_to: new Date(Number(params.year), 11, 31, 23, 59, 59).getTime() / 1000,
+        date_from: new Date(Number(year), 0, 1, 0, 0, 0, 0).getTime() / 1000,
+        date_to: new Date(Number(year), 11, 31, 23, 59, 59).getTime() / 1000,
         per_page: 100,
         industry_id: searchParams?.industry_id ? Number(searchParams.industry_id) : undefined
     }
@@ -77,14 +79,14 @@ export default async function SchedulePage({ params, searchParams }: Props) {
 
     return <div className="flex flex-col gap-6">
         <div className="flex gap-6 items-center justify-between">
-            <H1 className="text-center">План мероприятий на {params.year} год</H1>
+            <H1 className="text-center">План мероприятий на {year} год</H1>
 
             <div className="flex gap-2 flex-wrap">
                 {years.map((year) => (
                     <Link
                         key={year}
                         href={`/schedule/${year}`}
-                        className={cn(year === Number(params.year) && "bg-brand text-brand-foreground", year !== Number(params.year) && "bg-muted text-muted-foreground", "text-3xl px-4 py-2 rounded-lg hover:bg-brand hover:text-brand-foreground transition-all duration-300")}
+                        className={cn(year === Number(year) && "bg-brand text-brand-foreground", year !== Number(year) && "bg-muted text-muted-foreground", "text-3xl px-4 py-2 rounded-lg hover:bg-brand hover:text-brand-foreground transition-all duration-300")}
                     >
                         {year}
                     </Link>
@@ -92,8 +94,8 @@ export default async function SchedulePage({ params, searchParams }: Props) {
             </div>
         </div>
 
+        <Calendar industries={industries.data?.data ?? []} initialEvents={events.data?.data ?? []} params={requestParams} year={Number(year)} />
 
-        <Calendar industries={industries.data?.data ?? []} initialEvents={events.data?.data ?? []} params={requestParams} year={Number(params.year)} />
         <div className="prose max-w-none text-sm">
             <Content />
         </div>
