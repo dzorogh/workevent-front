6c409c818e346685724366c332576cf6c8fdf2ec
Sergey Indenbom <dzorogh@gmail.com>
2024-12-29 14:06:40 +0300

    refactor: comment out generateStaticParams in SchedulePage
    - Temporarily commented out the generateStaticParams function in the SchedulePage component, which includes the logic for fetching industry slugs and generating static parameters. This change may be part of a larger refactor or debugging effort to simplify the code and improve maintainability.

--- src/app/schedule/[year]/[[...industry]]/page.tsx
+++ src/app/schedule/[year]/[[...industry]]/page.tsx
@@ -24,13 +24,13 @@ const getYears = (startYear: number) => {
     return Array.from({ length: new Date().getFullYear() - startYear + 3 }, (_, i) => i + startYear);
 }
 
-export async function generateStaticParams() {
-    const years = getYears(startYear);
-    const industries = (await Api.GET('/v1/industries/slugs')).data?.data ?? [];
-    const industrySlugs = industries.map((industry) => industry.slug);
+// export async function generateStaticParams() {
+//     const years = getYears(startYear);
+//     const industries = (await Api.GET('/v1/industries/slugs')).data?.data ?? [];
+//     const industrySlugs = industries.map((industry) => industry.slug);
 
-    return years.flatMap((year) => industrySlugs.map((industrySlug) => ({ year: year.toString(), industry: [industrySlug] })));
-}
+//     return years.flatMap((year) => industrySlugs.map((industrySlug) => ({ year: year.toString(), industry: [industrySlug] })));
+// }
 
 const getPage = async () => {
     const pageResponse = await Api.GET('/v1/pages', {
