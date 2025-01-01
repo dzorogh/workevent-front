01ff2718228823b96d5627b6d324a004cda81cf7
Sergey Indenbom <dzorogh@gmail.com>
2025-01-01 22:31:48 +0300

    feat: implement slug generation for blog post URLs
    - Introduced createSlugWithId utility to generate slugs for blog post links, enhancing SEO and user experience.
    - Updated BlogPage to use slugs in post links instead of IDs.
    - Enhanced BlogPostPage to validate slugs and redirect if necessary, ensuring users land on the correct post URL.
    - Improved error handling by implementing notFound and permanentRedirect for better navigation.

--- src/app/blog/[post]/page.tsx
+++ src/app/blog/[post]/page.tsx
@@ -5,6 +5,8 @@ import Link from 'next/link';
 import { compile, run } from '@mdx-js/mdx'
 import * as runtime from 'react/jsx-runtime'
 import Image from 'next/image'
+import { getIdFromSlug, createSlugWithId } from '@/lib/utils';
+import { notFound, permanentRedirect } from 'next/navigation';
 
 const getPost = async (id: number) => {
     const response = await Api.GET(`/v1/posts/{post}`, {
@@ -17,12 +19,22 @@ const getPost = async (id: number) => {
     return response.data;
 }
 
-export default async function BlogPostPage({ params }: { params: { post: string } }) {
-    const post = await getPost(Number(params.post));
+export default async function BlogPostPage({ params }: { params: Promise<{ post: string }> }) {
+    const { post } = await params;
+    const postData = await getPost(Number(getIdFromSlug(post)));
+
+    if (!postData) {
+        notFound();
+    }
+
+    const correctSlug = createSlugWithId(postData.data.title, postData.data.id);
+    if (post !== correctSlug) {
+        permanentRedirect(`/blog/${correctSlug}`);
+    }
 
     // Compile the MDX source code to a function body
     const code = String(
-        await compile(post?.data?.content ?? '', { outputFormat: 'function-body' })
+        await compile(postData?.data?.content ?? '', { outputFormat: 'function-body' })
     )
 
     // Run the compiled code with the runtime and get the default export
@@ -42,10 +54,10 @@ export default async function BlogPostPage({ params }: { params: { post: string 
             </div>
 
             <div className="max-w-3xl mx-auto p-6 bg-white rounded-lg">
-                <h1 className="text-4xl font-bold mb-4">{post?.data?.title}</h1>
-                <p className="text-gray-600 mb-4">Опубликовано {new Date(post?.data?.created_at || '').toLocaleDateString('ru-RU', { day: 'numeric', month: 'long', year: 'numeric' })}</p>
+                <h1 className="text-4xl font-bold mb-4">{postData?.data?.title}</h1>
+                <p className="text-gray-600 mb-4">Опубликовано {new Date(postData?.data?.created_at || '').toLocaleDateString('ru-RU', { day: 'numeric', month: 'long', year: 'numeric' })}</p>
                 <div className="relative aspect-video w-full border-secondary border rounded-lg overflow-hidden bg-muted">
-                    <Image src={post?.data?.cover || ''} alt="Blog Post" sizes="1600px" fill />
+                    <Image src={postData?.data?.cover || ''} alt="Blog Post" sizes="1600px" fill />
                 </div>
                 <div className="prose max-w-none text-md">
                     <Content />
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,6 +4,7 @@ import H1 from '@/components/ui/h1';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import Image from 'next/image';
+import { createSlugWithId } from '@/lib/utils';
 
 const getPosts = async () => {
     const response = await Api.GET('/v1/posts');
@@ -18,7 +19,7 @@ export default async function BlogPage() {
             <H1>Блог</H1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {posts?.data?.map(post => (
-                    <Link href={`/blog/${post.id}`} key={post.id} className="rounded-lg flex flex-col gap-4">
+                    <Link href={`/blog/${createSlugWithId(post.title, post.id)}`} key={post.id} className="rounded-lg flex flex-col gap-4">
                         <div className="relative aspect-video w-full border-secondary border rounded-lg overflow-hidden bg-muted">
                             <Image src={post.cover} alt={post.title} sizes="500px" fill />
                         </div>
