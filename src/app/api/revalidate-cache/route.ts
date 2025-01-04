e34e91f9d0eaf50c806703efcd1d74b38bb77d2c
Sergey Indenbom <dzorogh@gmail.com>
2025-01-04 16:59:21 +0300

    feat: add secret validation for cache revalidation route

--- src/app/api/revalidate-cache/route.ts
+++ src/app/api/revalidate-cache/route.ts
@@ -15,6 +15,10 @@ export async function GET(request: NextRequest) {
     sha256.update(secret);
     const hash = sha256.digest('hex');
 
+    if (!process.env.NEXT_CACHE_REVALIDATE_SECRET_HASH) {
+        return Response.json({ message: 'Secret is not set' }, { status: 500 })
+    }
+
     // validate secret
     if (process.env.NEXT_CACHE_REVALIDATE_SECRET_HASH !== hash) {
         return Response.json({ message: 'Invalid secret' }, { status: 401 })
