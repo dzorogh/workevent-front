6385c5f47c657c0304ab7dc20b5d8ba6d9012020
Sergey Indenbom <dzorogh@gmail.com>
2025-01-04 12:37:36 +0300

    fix: update menu link text for clarity
    - Changed the link text for the schedule page from "Расписание на 2025 год" to "Календарь" to improve user understanding and navigation.

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -5,7 +5,7 @@ export default function Menu({ isMobile = false }: { isMobile?: boolean }) {
     <nav>
         <ul className={`flex gap-x-6 gap-y-2 ${isMobile ? 'flex-col' : ''}`}>
             <li><AppLink href="/events">Мероприятия</AppLink></li>
-            <li><AppLink href={{ pathname: "/schedule/2025" }}>Расписание на 2025 год</AppLink></li>
+            <li><AppLink href={{ pathname: "/schedule/2025" }}>Календарь</AppLink></li>
             <li><AppLink href="/blog">Блог</AppLink></li>
         </ul>
     </nav>
